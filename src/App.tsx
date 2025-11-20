484cd8170c25ef020ba48c2becf09805a3e4d75e
slavic <tkac05625@gmail.com>
2025-11-20 18:51:56 +0300

    добавил простой роутинг в проект

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@
 import { useState, type MouseEvent } from 'react'
 import Button from './components/Button/Button'
 import Input from './components/Input/Input';
+import { Route, Routes } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/ErrorPage/Error';
 
 function App() {
 
@@ -17,6 +21,15 @@ function App() {
       <Button onClick={addCounter}>Кнопка</Button>
       <Button appearence='big' onClick={addCounter}>Кнопка</Button>
       <Input placeholder='email'></Input>
+      <div>
+        <a href="/">Меню</a>
+        <a href="/cart">Корзина</a>
+      </div>
+      <Routes>
+        <Route path='/' element={<Menu></Menu>}></Route>
+        <Route path='/cart' element={<Cart></Cart>}></Route>
+        <Route path='*' element={<Error></Error>}></Route>
+      </Routes>
     </>
   )
 }
